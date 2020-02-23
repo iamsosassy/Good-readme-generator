23e5fa6936fddbafbeff9464518ed2ef4cf43aed
Jersey <shirleyrivera70@gmail.com>
2020-02-23 18:00:33 -0500

    added questions to file

--- index.js
+++ index.js
@@ -1,54 +1,80 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const axios = require("axios");
+//const axios = require("axios");
+
+//https://api.github.com/users/iamsosassy?client_id=Iv1.67dd2ecaeaf37952&client_secret=de0ace8451d206441f5001cd61a975334fa54758
+
+console.log('hellooo!');
 
 inquirer.prompt([{
-                type: "input",
-                message: "What is your GitHub user name?",
-                name: "username"
-            },
-            {
-                type: "input",
-                message: "What is your project's name?",
-                name: "project"
-            },
-            {
-                type: "input",
-                message: "Please write a short description of your project.",
-                name: "description"
-            },
-            {
-                type: "list",
-                message: "What kind of license should your project have?",
-                choices: ["BSD", "Apache", "Eclipse"],
-                name: "licensetype"
-            },
-            {
-                type: "input",
-                message: "What command should be run to install dependencies?",
-                name: "dependencies",
-                default: "npm i"
-            },
-            {
-                type: "input",
-                message: "What command should be run to run tests?",
-                name: "tests",
-                default: "npm test"
-            },
-            {
-                type: "input",
-                message: "What does the user need to know about using the repo?",
-                name: "about"
-            },
-            {
-                type: "input",
-                message: "What does the user need to know about contributing to the repo?",
-                name: "contributing"
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "username"
+    },
 
-                // function writeToFile(fileName, data) {}
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your project's name?",
+        name: "project"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project.",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        choices: ["BSD", "Apache", "Eclipse"],
+        name: "licensetype"
+    },
 
-                // function init() {
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "about"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contributing"
+    }
+]).then(function(answers) {
+    console.log('this is answer to our questions', answers);
 
-                // }
+    var readMeFile = `
+    This is our awesome readme for our project
 
-                // init();
+        Username : ${answers.username}
+        email: ${answers.email}
+        project: ${answers.project}
+        description: ${answers.description}
+        licensetype: ${answers.licensetype}
+        about: ${answers.about}
+        contributing: ${answers.contributing}
+    `;
+
+    //fs.writeFile('./', 'test.md', readMeFile)
+
+    fs.writeFile('test.md', readMeFile, function(err) {
+        if (err) throw err;
+        console.log('Replaced!');
+    });
+
+
+
+
+})
+
+// function writeToFile(fileName, data) {}
+
+// function init() {
+
+// }
+
+// init();
---
+++ test.md
@@ -0,0 +1,12 @@
+
+    This is our awesome readme for our project
+
+    Username : asdf
+    email: asdf
+    project: asdf
+    desciption: asdf
+    licensetype: BSD
+    about: afd
+    contributing: asfds
+    
+    
