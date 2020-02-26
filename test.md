fc1891ade2063ecd39d70744e2bcc72ba8ea07da
Jersey <shirleyrivera70@gmail.com>
2020-02-25 22:56:43 -0500

    added data info on generatemarkdown and api

--- test.md
+++ test.md
@@ -1,12 +1,11 @@
 
     This is our awesome readme for our project
 
-    Username : asdf
-    email: asdf
-    project: asdf
-    desciption: asdf
-    licensetype: BSD
-    about: afd
-    contributing: asfds
-    
+        Username : iamsosassy
+        email: shirleyrivera70@gmail.com
+        project: Trying to create a readme
+        description: It suppose to generate a nice readable readme about me
+        licensetype: BSD
+        about: its at beginners level
+        contributing: help yourself to make it better
     
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,38 @@
 function generateMarkdown(data) {
-  return `
-# ${data.title}
+    return `
+# ${data.title} ![GitHub package.json version](https://img.shields.io/github/package-json/v/Iamsosassy/Good-README-Generator)
+${data.description}
+# Tables of Contents
+* [Username](#username)
+* [Email](#email)
+* [Project](#project)
+* [Description](#description)
+* [Licensetype](#licensetype)
+* [About](#About)
+* [Contributing](#contributing)
 
+# Username
+${data.username}
+# Email
+${data.email}
+# Project
+${data.project}
+# Description
+${data.description}
+# Licensetype
+${data.licensetype}
+# About
+${data.about}
+# Contributing
+${data.contributing}
+
+# Questions
+![Profile Avatar](${data.pfp})
+If you have any questions, please e-mail me at ${data.email}.
+# Credits
+Github: [${data.username}](${data.url})
+Copyright ${data.name}. All Rights Reserved.
 `;
-}
+};
 
 module.exports = generateMarkdown;
